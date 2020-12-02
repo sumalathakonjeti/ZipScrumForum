1a955192489fea92d3b2af0ca78213378c918f57
Shenghao <itzshengh95@gmail.com>
2020-12-02 15:17:17 -0500

    First Commit

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+SECRET_KEY = os.environ['SECRET_KEY']
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SITE_NAME = "Default Forum Name"
+SITE_DESCRIPTION = "Change this value in config.py"
+username = os.getenv('MYSQL_user')
+pswd = os.getenv('MYSQL_pw')
+#SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/database.db";
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://' + username + ":" + pswd + '@localhost/posts'
+
